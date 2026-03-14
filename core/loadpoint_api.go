bb6e6c83f8280368c9b833d007d41a874bc7090a
Dodi1968 <d.disse@gmx.de>
2026-03-14 23:04:28 +0000

    wip

--- core/loadpoint_api.go
+++ core/loadpoint_api.go
@@ -477,7 +477,7 @@ func (lp *Loadpoint) SetGeofenceEnabled(enabled bool) {
 	lp.Lock()
 	defer lp.Unlock()
 
-	lp.log.DEBUG.Printf("set geofence enabled: %v -> %v", lp.GeofenceEnabled, enabled)
+	lp.log.DEBUG.Printf("set geofence enabled: %v", enabled)
 
 	lp.GeofenceEnabled = enabled
 	lp.settings.SetBool(keys.GeofenceEnabled, enabled)
@@ -500,7 +500,7 @@ func (lp *Loadpoint) SetGeofenceLat(lat float64) {
 	lp.Lock()
 	defer lp.Unlock()
 
-	lp.log.DEBUG.Printf("set geofence latitude: %.4f -> %.4f", lp.GeofenceLat, lat)
+	lp.log.DEBUG.Printf("set geofence latitude: %.4f", lat)
 
 	lp.GeofenceLat = lat
 	lp.settings.SetFloat(keys.GeofenceLat, lat)
@@ -523,7 +523,7 @@ func (lp *Loadpoint) SetGeofenceLon(lon float64) {
 	lp.Lock()
 	defer lp.Unlock()
 
-	lp.log.DEBUG.Printf("set geofence longitude: %.4f -> %.4f", lp.GeofenceLon, lon)
+	lp.log.DEBUG.Printf("set geofence longitude: %.4f", lon)
 
 	lp.GeofenceLon = lon
 	lp.settings.SetFloat(keys.GeofenceLon, lon)
@@ -546,7 +546,7 @@ func (lp *Loadpoint) SetGeofenceRadius(radius float64) {
 	lp.Lock()
 	defer lp.Unlock()
 
-	lp.log.DEBUG.Printf("set geofence radius: %v -> %v", lp.GeofenceRadius, radius)
+	lp.log.DEBUG.Printf("set geofence radius: %v", radius)
 
 	lp.GeofenceRadius = radius
 	lp.settings.SetFloat(keys.GeofenceRadius, radius)
